cc0dbaf484cd1aa79a60e9a8d177a5c0e7f84fcc
Prince Harry Nunoo <hpsn16@gmail.com>
2024-06-01 18:46:28 +0000

    Added new database models and updated database connection
    A new folder `Database\Base_Models\` was added to the project structure and the database connection string was updated to use `demo_airlines`. A new schema design was proposed and implemented with new classes to represent the base models of the database. The `DemoAirlinesContext` class was added to represent the DbContext of the application, including DbSet properties for each entity and methods to configure the database and entity types. The command to execute the SQL script and scaffold the DbContext were updated accordingly.

---
+++ Airlines.Presentation/AirlinesApi/Database/Base_Models/Aircraft.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AirlinesApi.Database.Base_Models;
+
+public partial class Aircraft
+{
+    /// <summary>
+    /// Aircraft code, IATA
+    /// </summary>
+    public string? AircraftCode { get; set; }
+
+    /// <summary>
+    /// Aircraft model
+    /// </summary>
+    public string? Model { get; set; }
+
+    /// <summary>
+    /// Maximal flying distance, km
+    /// </summary>
+    public int? Range { get; set; }
+}
---
+++ Airlines.Presentation/AirlinesApi/Database/DbContexts/AirlinesContext.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using AirlinesApi.Database.Base_Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AirlinesApi.Database.DbContexts;
+
+public partial class AirlinesContext : DbContext
+{
+    public AirlinesContext()
+    {
+    }
+
+    public AirlinesContext(DbContextOptions<AirlinesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Traveller> Travellers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseNpgsql("Name=ConnectionStrings:Database");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Traveller>(entity =>
+        {
+            entity.ToTable("Travellers", "bookings");
+
+            entity.HasIndex(e => e.NormalizedEmail, "EmailIndex");
+
+            entity.HasIndex(e => e.NormalizedUserName, "idx_username").IsUnique();
+
+            entity.Property(e => e.Id)
+                .HasMaxLength(25)
+                .IsFixedLength();
+            entity.Property(e => e.Country).HasMaxLength(35);
+            entity.Property(e => e.Email).HasMaxLength(256);
+            entity.Property(e => e.NormalizedEmail).HasMaxLength(40);
+            entity.Property(e => e.NormalizedUserName).HasMaxLength(256);
+            entity.Property(e => e.PassengerName)
+                .HasMaxLength(75)
+                .HasColumnName("passenger_name");
+            entity.Property(e => e.PhoneNumber).HasMaxLength(15);
+            entity.Property(e => e.UserName).HasMaxLength(40);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
