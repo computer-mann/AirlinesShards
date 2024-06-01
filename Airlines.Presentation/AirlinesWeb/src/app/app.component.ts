672eab5ed8062a91b8e1a944816e0d9c0fc4305b
Prince Harry Nunoo <hpsn16@gmail.com>
2024-06-01 17:52:37 +0000

    angular app

--- Airlines.Presentation/AirlinesWeb/src/app/app.component.ts
+++ Airlines.Presentation/AirlinesWeb/src/app/app.component.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import {HubConnection, HubConnectionBuilder} from '@microsoft/signalr';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'nunoo-airlines';
+  title = 'Airlines Web';
 }
