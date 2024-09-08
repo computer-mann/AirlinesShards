531c7d467d9f6974fb3042c6aab6a24122e14c3c
Prince Harry Nunoo <hpsn16@gmail.com>
2024-09-08 17:01:56 +0000

    itestcollection learned today

---
+++ Tests.Integration.Airlines/AirlineApplicationFactory.cs
@@ -0,0 +1,28 @@
+﻿using AirlinesApi;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.Integration.Airlines
+{
+    internal class AirlineApplicationFactory:WebApplicationFactory<Program>
+    {
+
+        public AirlineApplicationFactory()
+        {
+            
+        }
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureAppConfiguration((context, config) =>
+            {
+                
+            });
+        }
+    }
+}
