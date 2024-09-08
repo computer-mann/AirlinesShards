a15e98164db51a41e93bfb615520a95fca7501d6
Prince Harry Nunoo <hpsn16@gmail.com>
2024-09-08 17:21:39 +0000

    configure app configuration

--- Tests.Integration.Airlines/AirlineApplicationFactory.cs
+++ Tests.Integration.Airlines/AirlineApplicationFactory.cs
@@ -1,6 +1,7 @@
 ﻿using AirlinesApi;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,7 +22,13 @@ namespace Tests.Integration.Airlines
         {
             builder.ConfigureAppConfiguration((context, config) =>
             {
-                
+                config.AddInMemoryCollection([
+                    new KeyValuePair<string, string?>("ConnectionStrings:AirlineDb", "Host=localhost;Port=5432;Database=AirlineDb;Username=postgres;Password=postgres"),
+                    new KeyValuePair<string, string?>("ConnectionStrings:Seq", "http://localhost:5341"),
+                    new KeyValuePair<string, string?>("ConnectionStrings:Redis", "localhost:6379"),
+                    new KeyValuePair<string, string?>("ConnectionStrings:Jaeger", "localhost:6831"),
+                    new KeyValuePair<string, string?>("ConnectionStrings:Zipkin", "http://localhost:9411/api/v2/spans"),
+                    ]);
             });
         }
     }
