db207f19288d35933d22de6f815a1259b2c08607
Prince Harry Nunoo <princeharry.nunoo@hubtel.com>
2025-02-17 12:14:52 +0000

    full dotnet 9 version

--- Airlines.Presentation/AirlinesApi/Program.cs
+++ Airlines.Presentation/AirlinesApi/Program.cs
@@ -37,14 +37,6 @@ namespace AirlinesApi
         {
             
             var builder = WebApplication.CreateBuilder(args);
-            builder.WebHost.ConfigureKestrel((context, options) =>
-            {
-                options.ListenAnyIP(5001, listenOptions =>
-                {
-                    listenOptions.Protocols = HttpProtocols.Http1AndHttp2AndHttp3;
-                    listenOptions.UseHttps();
-                });
-            });
             Log.Logger = new LoggerConfiguration()
                 .ReadFrom.Configuration(builder.Configuration)
                 .WriteTo.File("SerilogLogs/log.txt", rollingInterval: RollingInterval.Hour)
